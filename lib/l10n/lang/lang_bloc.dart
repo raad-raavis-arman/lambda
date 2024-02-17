a333b9917311fb96df81c465f43544c5a42316f3
TalebRafiepour <taleb_r75@yahoo.com>
2024-02-17 12:01:05 +0330

    fix bug of running on iOS after adding multi language #4

--- lib/l10n/lang/lang_bloc.dart
+++ lib/l10n/lang/lang_bloc.dart
@@ -1,8 +1,8 @@
-import 'package:devicelocale/devicelocale.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:landa/l10n/l10n.dart';
+import 'package:language_code/language_code.dart';
 
 part 'lang_event.dart';
 part 'lang_state.dart';
@@ -10,9 +10,9 @@ part 'lang_state.dart';
 class LangBloc extends Bloc<LangEvent, LangState> {
   LangBloc() : super(const LangState(locale: Locale('fa'))) {
     on<LangFetchLocalEvent>((event, emit) async {
-      final Locale? locale = await Devicelocale.currentAsLocale;
+      final Locale locale = LanguageCode.locale;
 
-      final langauageCode = locale!.languageCode;
+      final langauageCode = locale.languageCode;
       if (L10n.all.contains(Locale(langauageCode))) {
         emit(LangState(locale: locale));
       }
