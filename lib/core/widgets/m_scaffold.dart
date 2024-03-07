0981a8101774e29927597e28bab53e52362cf17d
TalebRafiepour <taleb_r75@yahoo.com>
2024-03-07 12:45:53 +0330

    upadate my custom MScaffold

--- lib/core/widgets/m_scaffold.dart
+++ lib/core/widgets/m_scaffold.dart
@@ -1,31 +1,111 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class MScaffold extends Scaffold {
+class MScaffold extends StatelessWidget {
   const MScaffold({
     super.key,
-    super.appBar,
-    super.body,
-    super.floatingActionButton,
-    super.floatingActionButtonLocation,
-    super.floatingActionButtonAnimator,
-    super.persistentFooterButtons,
-    super.persistentFooterAlignment,
-    super.drawer,
-    super.onDrawerChanged,
-    super.endDrawer,
-    super.onEndDrawerChanged,
-    super.bottomNavigationBar,
-    super.bottomSheet,
-    super.backgroundColor,
-    super.resizeToAvoidBottomInset,
-    super.primary,
-    super.drawerDragStartBehavior,
-    super.extendBody,
-    super.extendBodyBehindAppBar,
-    super.drawerScrimColor,
-    super.drawerEdgeDragWidth,
-    super.drawerEnableOpenDragGesture,
-    super.endDrawerEnableOpenDragGesture,
-    super.restorationId,
+    this.appBar,
+    this.body,
+    this.floatingActionButton,
+    this.floatingActionButtonLocation,
+    this.floatingActionButtonAnimator,
+    this.persistentFooterButtons,
+    this.persistentFooterAlignment = AlignmentDirectional.centerEnd,
+    this.drawer,
+    this.onDrawerChanged,
+    this.endDrawer,
+    this.onEndDrawerChanged,
+    this.bottomNavigationBar,
+    this.bottomSheet,
+    this.backgroundColor,
+    this.resizeToAvoidBottomInset,
+    this.primary = true,
+    this.drawerDragStartBehavior = DragStartBehavior.start,
+    this.extendBody = false,
+    this.extendBodyBehindAppBar = false,
+    this.drawerScrimColor,
+    this.drawerEdgeDragWidth,
+    this.drawerEnableOpenDragGesture = true,
+    this.endDrawerEnableOpenDragGesture = true,
+    this.restorationId,
   });
+
+  final bool extendBody;
+
+  final bool extendBodyBehindAppBar;
+
+  final PreferredSizeWidget? appBar;
+
+  final Widget? body;
+
+  final Widget? floatingActionButton;
+
+  final FloatingActionButtonLocation? floatingActionButtonLocation;
+
+  final FloatingActionButtonAnimator? floatingActionButtonAnimator;
+
+  final List<Widget>? persistentFooterButtons;
+
+  final AlignmentDirectional persistentFooterAlignment;
+
+  final Widget? drawer;
+
+  final DrawerCallback? onDrawerChanged;
+
+  final Widget? endDrawer;
+
+  final DrawerCallback? onEndDrawerChanged;
+
+  final Color? drawerScrimColor;
+
+  final Color? backgroundColor;
+
+  final Widget? bottomNavigationBar;
+
+  final Widget? bottomSheet;
+
+  final bool? resizeToAvoidBottomInset;
+
+  final bool primary;
+
+  final DragStartBehavior drawerDragStartBehavior;
+
+  final double? drawerEdgeDragWidth;
+
+  final bool drawerEnableOpenDragGesture;
+
+  final bool endDrawerEnableOpenDragGesture;
+
+  final String? restorationId;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: key,
+      appBar: appBar,
+      body: body,
+      floatingActionButton: floatingActionButton,
+      floatingActionButtonLocation: floatingActionButtonLocation,
+      floatingActionButtonAnimator: floatingActionButtonAnimator,
+      persistentFooterButtons: persistentFooterButtons,
+      persistentFooterAlignment: persistentFooterAlignment,
+      drawer: drawer,
+      onDrawerChanged: onDrawerChanged,
+      endDrawer: endDrawer,
+      onEndDrawerChanged: onEndDrawerChanged,
+      bottomNavigationBar: bottomNavigationBar,
+      bottomSheet: bottomSheet,
+      backgroundColor: backgroundColor,
+      resizeToAvoidBottomInset: resizeToAvoidBottomInset,
+      primary: primary,
+      drawerDragStartBehavior: drawerDragStartBehavior,
+      extendBody: extendBody,
+      extendBodyBehindAppBar: extendBodyBehindAppBar,
+      drawerScrimColor: drawerScrimColor,
+      drawerEdgeDragWidth: drawerEdgeDragWidth,
+      drawerEnableOpenDragGesture: drawerEnableOpenDragGesture,
+      endDrawerEnableOpenDragGesture: endDrawerEnableOpenDragGesture,
+      restorationId: restorationId,
+    );
+  }
 }
