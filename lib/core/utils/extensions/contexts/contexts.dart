507b765c59ffac03602fd1d2d72dc1a43dac389c
TalebRafiepour <taleb_r75@yahoo.com>
2024-03-20 12:55:26 +0330

    add popup functionality #9

--- lib/core/utils/extensions/contexts/contexts.dart
+++ lib/core/utils/extensions/contexts/contexts.dart
@@ -1,2 +1,3 @@
 export 'context_font_sizes.dart';
+export 'context_layout_sizes.dart';
 export 'context_locale.dart';
--- lib/core/widgets/widgets.dart
+++ lib/core/widgets/widgets.dart
@@ -1,3 +1,4 @@
+export 'm_popup.dart';
 export 'm_scaffold.dart';
 export 'm_text.dart';
 export 'selectable/selectable.dart';
