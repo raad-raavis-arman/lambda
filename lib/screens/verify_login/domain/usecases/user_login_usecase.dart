210b93c1a659a7692ec3ed89ca30209f5bcb67bf
TalebRafiepour <taleb_r75@yahoo.com>
2024-03-25 20:45:29 +0330

    fix persian number in sending request to server #36

--- lib/screens/verify_login/domain/usecases/user_login_usecase.dart
+++ lib/screens/verify_login/domain/usecases/user_login_usecase.dart
@@ -2,27 +2,29 @@ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:landa/core/error/failure.dart';
 import 'package:landa/core/usecase/usecase.dart';
+import 'package:landa/core/utils/utils.dart';
 import 'package:landa/screens/verify_login/domain/entities/entities.dart';
 import 'package:landa/screens/verify_login/domain/repositories/repositories.dart';
 
 class UserLoginUsescase extends BaseUseCase<LoginAuth, LoginParam> {
-
   UserLoginUsescase({required this.userLoginRepository});
 
   final UserLoginRepository userLoginRepository;
 
   @override
   Future<Either<Failure, LoginAuth>> call(LoginParam params) =>
-      userLoginRepository.loginWithMobile(params.mobileNumber, params.otpCode);
+      userLoginRepository.loginWithMobile(
+        params.mobileNumber.replaceFaNumToEn(),
+        params.otpCode.replaceFaNumToEn(),
+      );
 }
 
 class LoginParam extends Equatable {
-
   const LoginParam({
     required this.mobileNumber,
     required this.otpCode,
   });
-  
+
   final String mobileNumber;
   final String otpCode;
 
