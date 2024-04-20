a1863db43efcf584b55855fe41f347296e8b724d
TalebRafiepour <taleb_r75@yahoo.com>
2024-04-20 22:53:32 +0330

    fix minor issue

--- lib/core/utils/extensions/durations/duration_properties_getter.dart
+++ lib/core/utils/extensions/durations/duration_properties_getter.dart
@@ -1,5 +1,5 @@
 extension DurationPropertiesGetter on Duration {
-  int get days => inDays;
+  int get days => inDays <= 0 ? 0 : inDays;
   int get hours => inHours <= 0 ? 0 : inHours % 24;
   int get minutes => inMinutes <= 0 ? 0 : inMinutes % 60;
   int get seconds => inSeconds <= 0 ? 0 : inSeconds % 60;
